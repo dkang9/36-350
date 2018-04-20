852dc1d34460ee4febcaefb92ff9e973ac25df60
Donghun <dkang9@gmail.com>
2018-04-19 21:29:37 -0400

    added model_select function

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,7 +1,14 @@
-generate_data(n,p) = function {
+generate_data = function(n,p) {
   covariates = matrix(rnorm(n*p,0,1),nrow=n,ncol=p)
   responses = rnorm(n,0,1)
   return(list(covariates,responses))
+}
+
+
+model_select = function(covariates,responses,cutoff) {
+  linear.regression = lm(responses ~ covariates)
+  linear.regression.low = covariates[linear.regression["p-value"] <= cutoff]
+  return(lm(responses ~ linear.regression.low))
 }
 
 
